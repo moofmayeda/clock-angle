d545b1d177842f0df3bff33245ac498ea9b82740
moof mayeda & Scott Dudley <student@epicodus.com>
2014-07-22 16:49:52 -0700

    Solved problem and refactored variable names

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -1,6 +1,9 @@
 def clock_angle(time)
   time_array = time.split(":")
-  time_array[0] = time_array[0].to_i
-  time_array[1] = time_array[1].to_i
-  (time_array[1]*6 - (time_array[0]*30 + time_array[1]/2)).abs
+  hours = time_array[0].to_i
+  minutes = time_array[1].to_i
+  angle = (minutes*6 - (hours*30.0 + minutes/2.0)).abs
+
+  angle > 180 ? 360 - angle : angle
+
 end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -2,7 +2,11 @@ require("rspec")
 require("clock_angle")
 
 describe("clock_angle") do
-  it("tells the difference in degrees between the minute and hour hand for when both hands are between 12 and 3") do
-    clock_angle("2:12").should(eq(6))
+  it("tells the difference in degrees between the minute and hour hand for when both hands are between 12 and 6") do
+    clock_angle("4:28").should(eq(34))
+  end
+
+  it("tells the difference in degrees between the minute and hour hand when they are on opposite sides of the clock") do
+    clock_angle("11:21").should(eq(145.5))
   end
 end
